56f3a425472bf520ad771b6f4b390b58e91dfc35
tpecherkina <tpecherkina18@gmail.com>
2022-01-21 18:06:31 -0500

    ci

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
 export default function sortArr(arr) {
-    return arr.sort((a,b) => {
-        return b.health - a.health 
-    }, 0);
-    return arr
+  return arr.sort((a, b) => b.health - a.health, 0);
+  return arr;
 }
